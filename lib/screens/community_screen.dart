754aaa7b4e062901f3d3e7a5a157c8e60cf180f3
seojm <myungs8562@gmail.com>
2023-11-20 19:05:57 +0900

    Add: 합치기 전

--- lib/screens/community_screen.dart
+++ lib/screens/community_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CommunityScreen extends StatelessWidget {
   const CommunityScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
