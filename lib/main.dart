1a169f215bfe5edf75d49a004c30555c799ede96
seojm <myungs8562@gmail.com>
2023-12-02 11:39:31 +0900

    Feat: Davin 합침

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-import 'package:haruhan/screens/community_screen.dart';
-import 'package:haruhan/screens/home_screen.dart';
+import 'package:haruhan/screens/login_screen.dart';
+import 'package:haruhan/screens/mainhome_screen.dart';
 import 'package:haruhan/screens/sign_up_screen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +17,7 @@ final _router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const CommunityScreen(),
+      builder: (context, state) => const LoginScreen(),
     ),
     GoRoute(
       path: '/signup',
@@ -27,7 +25,7 @@ final _router = GoRouter(
     ),
     GoRoute(
       path: '/home',
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => Mainhome(),
     ),
   ],
 );
@@ -39,7 +37,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,12 +8,15 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
+    bool isBook1Selected = false;
+    bool isBook2Selected = false;
 
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
             await FirebaseAuth.instance.signOut();
+            context.go('/');
           },
           child: Text("로그아웃"),
         ),
